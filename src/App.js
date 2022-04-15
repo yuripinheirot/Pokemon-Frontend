572ff46da9790630167e66007013a257afee75ba
Yuri Pinheiro <yuripinheirot@gmail.com>
2022-04-15 00:38:36 -0300

    Added eslint

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
-function App() {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  );
-}
+import React from "react";
 
-export default App;
+export default function App() {
+	return <div className='App'>Hello World</div>;
+}
