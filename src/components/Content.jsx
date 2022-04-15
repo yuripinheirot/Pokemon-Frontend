14676cb8f3f677a9c27ae209b86e632dc46ec66f
Yuri Pinheiro <yuripinheirot@gmail.com>
2022-04-15 01:31:33 -0300

    created default content

---
+++ src/components/Content.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import { dark } from "constants/colors";
+
+const DivContent = styled.div `
+	background-color: ${dark.background.default};
+	height: 100%;
+	width: 100%;
+`;
+
+const Content = ({ id, children, ...props }) => {
+	return (
+		<DivContent  id={id} {...props}>
+			{children}
+		</DivContent>
+	);
+};
+
+Content.propTypes = {
+	children: PropTypes.node,
+	props: PropTypes.object,
+	id: PropTypes.string.isRequired,
+};
+
+export default Content;
