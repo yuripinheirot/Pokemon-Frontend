dbd2ae884bbe869afc8ae808f1a0649d617a29bb
Yuri Pinheiro <yuripinheirot@gmail.com>
2023-10-31 23:32:19 -0400

    fix: remove unecessary imports

--- src/pages/private/pokedex/containers/Pokedex.tsx
+++ src/pages/private/pokedex/containers/Pokedex.tsx
@@ -1,10 +1,7 @@
 import { Paper } from '@mui/material'
-import { useKeycloak } from '@react-keycloak/web'
 import React from 'react'
 
 const PokedexContainer = () => {
-  const { keycloak, initialized } = useKeycloak()
-
   return <Paper>PokedexContainer</Paper>
 }
 
